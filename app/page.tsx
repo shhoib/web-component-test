e453e0ea362df0342ea9b69b59dcfa7a9240132e
shuaib-devnation <shuaib.salam@devnation.be>
2025-11-20 17:42:20 +0530

    made a new project

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+  <div className="flex justify-center items-center h-screen">
+    <div className="bg-red-500 h-40 w-40"></div>
+  </div>
+  );
+}
