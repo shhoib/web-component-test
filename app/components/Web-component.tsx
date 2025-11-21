9e854f7ac7396c5b8142ad22e5bff71c699b1f28
shhoib <shuaibsalam1234@gmail.com>
2025-11-21 13:47:03 +0530

    ade an extra container

--- app/components/Web-component.tsx
+++ app/components/Web-component.tsx
@@ -2,6 +2,7 @@ const WebComponent = () => {
   return (
     <div className="flex justify-center items-center h-screen">
       <div className="bg-red-500 h-40 w-40"></div>
+      <div className="bg-yellow-500 h-40 w-40"></div>
     </div>
   );
 };
